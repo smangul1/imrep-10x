41e6b918848bdec4045096d67a860348cc0a91c7
Serghei Mangul <smangul@ucla.edu>
2018-01-15 10:15:55 -0800

    major changes. Next step add VDJ

--- imrep-10x_step2.py
+++ imrep-10x_step2.py
@@ -2,6 +2,9 @@ import csv
 import argparse
 from Levenshtein import distance
 import os
+import sys
+import collections
+
 
 def pairwise_distance(l):
     d_list=[]
@@ -43,18 +46,22 @@ for line in reader:
 file.close()
 
 
-print "Number barcodes", len(barcodes)
+print ("Number barcodes", len(barcodes))
 
 for b in barcodes:
     dict_TCR[b]=([],[])
 
 file=open(args.inFile)
 reader=csv.reader(file)
+next(reader,None)
+
 for line in reader:
+
+
     type=line[0]
     b=line[1]
     cdr3=line[3]
-    
+
     if type=="TRA":
         dict_TCR[b][0].append(cdr3)
     elif type=="TRB":
@@ -64,65 +71,71 @@ for line in reader:
 file.close()
 
 file=open(args.out+".ed","w")
-file2=open(args.out,"w")
-file3=open(args.out+".2.csv","w")
+file_unique=open(args.out+".unique.CDR3.csv","w")
+file_multiple_CDR3=open(args.out+".multiple.CDR3.csv","w")
+
 
 
 #file - with only barcode with >1 CDR3s
 file.write("sample,chain type, barcode, CDR3s, min ed")
 file.write("\n")
 
-#file2 - CDR3 are presented in filed separated by ;
-file2.write("sample,chain type, barcode, CDR3s, number of CDR3 for this barcode")
-file2.write("\n")
+#file_unique
+file_unique.write("sample,chain type, barcode, CDR3, nReads")
+file_unique.write("\n")
 
 
-#file3 - one CDR3 per line
-file3.write("sample,chain type, barcode, CDR3, number of CDR3 for this barcode")
-file3.write("\n")
+#file_multiple_CDR3
+file_multiple_CDR3.write("sample,chain type, barcode, CDR3, nReads")
+file_multiple_CDR3.write("\n")
 
 ##sample,chain type, barcode, CDR3, number of CDR3 for this barcode
 
-for key,value in dict_TCR.iteritems():
-    if len(set(value[0]))>=1:
-        list_temp=list(set(value[0]))
-        tmp=";".join(list(set(value[0])))
-        
-        if len(set(value[0]))>1:
-            file.write(sample+",TCRA"+","+str(key)+","+tmp +"," + str(pairwise_distance(list_temp)) )
-            file.write("\n")
-    
-    
-        file2.write(sample+",TCRA,"+str(key)+","+tmp+","+str(len(list_temp)))
-        file2.write("\n")
+for key,value in dict_TCR.items():
 
-        for i in list_temp:
-            file3.write(sample+",TCRA,"+str(key)+","+str(i)+","+str(len(list_temp)))
-            file3.write("\n")
-
-    
-    if len(set(value[1]))>=1:
-        list_temp=list(set(value[1]))
-        tmp=";".join(list(set(value[1])))
-        
-        if len(set(value[1]))>1:
-            file.write(sample+",TCRB"+","+str(key)+","+tmp +"," + str(pairwise_distance(list_temp)) )
-            file.write("\n")
-
-        file2.write(sample+",TCRB,"+str(key)+","+tmp+","+str(len(list_temp)))
-        file2.write("\n")
-
-        for i in list_temp:
-            file3.write(sample+",TCRB,"+str(key)+","+str(i)+","+str(len(list_temp)))
-            file3.write("\n")
+    counter_TCRA = collections.Counter(value[0])
+    counter_TCRB = collections.Counter(value[1])
 
 
 
 
+
+
+
+    if len(counter_TCRA)==1:
+
+        cdr3_TCRA=next (iter (counter_TCRA.keys()))
+        count_TCRA=next (iter (counter_TCRA.values()))
+
+        file_unique.write(sample + ",TCRA," + str(key) + ","+str(cdr3) + "," + str(count))
+        file_unique.write("\n")
+
+    if len(counter_TCRA) > 1:
+        for key2, value2 in counter_TCRA.items():
+            cdr3 = key2
+            count = value2
+            file_multiple_CDR3.write(sample + ",TCRA," + str(key) + "," + str(cdr3_TCRA) + "," + str(count_TCRA))
+            file_multiple_CDR3.write("\n")
+
+    if len(counter_TCRB) == 1:
+        cdr3_TCRB = next(iter(counter_TCRB.keys()))
+        count_TCRB = next(iter(counter_TCRB.values()))
+
+        file_unique.write(sample + ",TCRB," + str(key) + "," + str(cdr3_TCRB) + "," + str(count_TCRB))
+        file_unique.write("\n")
+
+    if len(counter_TCRB) > 1:
+        for key2, value2 in counter_TCRB.items():
+            cdr3 = key2
+            count = value2
+            file_multiple_CDR3.write(sample + ",TCRB," + str(key) + "," + str(cdr3) + "," + str(count))
+            file_multiple_CDR3.write("\n")
 
 file.close()
-file2.close()
-file2.close()
+file_unique.close()
+file_multiple_CDR3.close()
+
+
 
 	
 
